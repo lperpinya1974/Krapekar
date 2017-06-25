893e73c645d027db93e8a9472c921a5fac284dd8
lperpinya1974 <lperpinya1974@gmail.com>
2017-06-25 13:22:10 +0200

    Gauss

---
+++ src/gauss/Gauss.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gauss;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lluis_2
+ */
+public class Gauss {
+    
+    private static int testNumber;
+    private static int error;
+    private static Scanner keyboard;
+    
+    public static void main (String[] args)
+    {
+        keyboard = new Scanner(System.in);
+        while (readData())
+        {
+           System.out.println (testCase());
+        }
+    }
+
+    public static boolean readData() {
+        
+        testNumber = keyboard.nextInt();
+        error = keyboard.nextInt();
+        
+        if (testNumber == 0 && error ==0) return false;
+        else
+            return true;
+    }
+
+    public static String testCase() {
+        
+        int numberOfPrimes = 0;
+        double calculatedError, estimatedError;
+        String message;
+        
+        numberOfPrimes = calculateNumberOfPrimes();
+        calculatedError = calculateGaussAprox();
+        estimatedError = Math.abs((double) (calculateNumberOfPrimes()*1.0/testNumber) - calculateGaussAprox());
+       
+       
+                
+        message = (estimatedError>1./Math.pow(10, error))?  "Mayor" : "Menor";
+        return message;
+        
+    }
+    
+    public static int calculateNumberOfPrimes()
+    {
+        int numberOfPrimes = 0;
+        for (int number=2; number<testNumber;number++)
+        {
+            if (isPrime(number)) numberOfPrimes++;
+        }
+        return numberOfPrimes;
+    }
+    
+    public static boolean isPrime(int number)
+    {
+        for (int cont=2; cont<=Math.sqrt(number); cont++)
+        {
+            if (number%cont == 0) return false;
+            
+        }
+        return true;
+    }
+    
+    public static double calculateGaussAprox()
+    {
+        return 1/Math.log(testNumber);
+    }
+    
+    
+}
